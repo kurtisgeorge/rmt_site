a40acea8c5748c09484079e534f98035e72f7fd7
Kurt George <128714102+kurtisgeorge@users.noreply.github.com>
2026-03-17 17:23:19 -0600

    implemented github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/rmt_site/' : '/',
   plugins: [
     tailwindcss(),
     react({
@@ -12,4 +13,4 @@ export default defineConfig({
       },
     }),
   ],
-})
+}))
