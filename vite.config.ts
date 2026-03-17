71705b90413c63d6d5c8a61169bd09895c6a1c78
Kurt George <128714102+kurtisgeorge@users.noreply.github.com>
2026-03-17 17:31:02 -0600

    fixed gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/rmt_site/' : '/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     tailwindcss(),
     react({
